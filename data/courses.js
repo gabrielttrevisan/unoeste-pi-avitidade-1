98b1a8cee3ce65a814f784936b4a1c86b1474ebd
Gabriel Troian Trevisan <gabrieltr@outlook.com>
2026-02-13 18:40:34 -0300

    feat(wip): course details page

--- data/courses.js
+++ data/courses.js
@@ -13,6 +13,14 @@ const COURSES = {
     startAt: "10/04",
     duration: "6 meses",
     highlight: false,
+    description: `
+      <p>
+      In pellentesque dolor est, efficitur hendrerit dolor fermentum aliquet. Aenean non posuere nisi, et luctus magna. Integer sed congue dui, quis aliquet quam. Aenean risus sem, maximus eu metus id, consequat euismod lectus. Fusce interdum id ex a semper. Etiam augue odio, malesuada eu ullamcorper quis, interdum et lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas mi purus, lobortis at faucibus sollicitudin, dapibus eu mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus efficitur lacus a volutpat. Nullam facilisis felis lorem, a elementum lorem imperdiet a. Sed egestas consequat purus, ut eleifend nibh mattis ac. Pellentesque elementum, neque in efficitur aliquam, elit massa tincidunt metus, nec dapibus orci lectus id quam. Nam auctor at quam nec dictum. Fusce tortor justo, congue ac accumsan non, vestibulum sit amet massa.
+      </p>
+      <p>
+      Proin viverra condimentum magna, convallis tristique ligula auctor vel. Praesent aliquet neque quis massa aliquet congue. Nullam tincidunt mauris ut sem mattis, at rhoncus augue dictum. Etiam tincidunt sit amet eros quis blandit. Suspendisse euismod malesuada nisl feugiat semper. Nulla facilisi. Vestibulum vel odio blandit, tincidunt dolor vel, ultricies enim. Sed sed magna vestibulum, blandit quam et, tincidunt ante.
+      </p>
+    `,
   },
   "captura-e-domesticacao-de-fadas-curso-definitivo": {
     title: "Captura e Domesticação de fadas: Curso Definitivo",
@@ -22,6 +30,17 @@ const COURSES = {
     price: { discounted: "R$ 256.45", full: "R$ 326.45" },
     startAt: "25/05",
     duration: "8 meses",
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse accumsan sollicitudin sapien vel hendrerit. Quisque semper venenatis faucibus. Fusce commodo urna quis rutrum sollicitudin. Mauris purus mauris, sodales sit amet nulla feugiat, vehicula feugiat metus. Ut luctus purus ligula, ut sagittis diam semper sed. Quisque vulputate diam vitae venenatis tincidunt. Nunc non finibus ante.
+      </p>
+      <p>
+        In varius libero id tortor aliquet ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc nec ipsum aliquam, pulvinar risus sit amet, rutrum dui. Cras vitae nibh at eros imperdiet ultrices at eget sem. Etiam dapibus imperdiet ipsum. Morbi elit nulla, iaculis id mauris ac, pretium vehicula velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam euismod posuere nulla nec ornare. Phasellus euismod eleifend purus ac molestie. Maecenas sit amet diam sollicitudin, maximus mauris ac, suscipit mauris. Proin porttitor ante urna, sed hendrerit erat finibus quis. In interdum justo et nibh euismod, sit amet rutrum metus rutrum. Nunc vulputate ipsum ac felis dictum, sit amet ultricies massa bibendum. Aenean imperdiet sagittis mi, eu interdum nisi scelerisque et. Sed velit quam, aliquet a augue at, interdum tincidunt nisi.
+      </p>
+      <p>
+        Nunc eget aliquam arcu. In quis nunc non neque consequat varius a non augue. Aenean congue tempor diam non pharetra. Nunc elementum luctus libero at malesuada. Phasellus sodales dolor rutrum, malesuada neque quis, aliquam ante. Nunc hendrerit orci non mauris feugiat, nec congue urna porta. Cras ut enim at odio sodales consectetur. Donec hendrerit felis eros, nec mattis diam elementum vitae. Donec tempor eu lorem pulvinar ornare. Curabitur eleifend massa at felis elementum faucibus.
+      </p>
+    `,
   },
   "identificacao-e-classificacao-de-repitilianos": {
     title: "Identificação e Classificação de Repitilianos",
@@ -31,6 +50,17 @@ const COURSES = {
     price: { discounted: "R$ 56.90", full: "R$ 86.90" },
     startAt: "08/03",
     duration: "uma semana",
+    description: `
+      <p>
+        Maecenas eleifend ut tellus eget cursus. Cras ut arcu sed sapien scelerisque cursus. Nullam accumsan sodales ligula, id malesuada libero elementum sed. Phasellus posuere nulla risus, a sollicitudin eros pretium eget. Aenean sed massa malesuada, rutrum magna posuere, consequat nisi. Proin at ultrices nunc, eget scelerisque urna. Fusce et scelerisque urna. Ut id sapien vel quam fermentum accumsan. Phasellus cursus neque nec cursus convallis.
+      </p>
+      <p>
+        Ut ultrices enim non tortor ultricies laoreet. Donec felis ante, euismod quis turpis a, scelerisque sagittis mi. Aliquam eget erat suscipit, bibendum libero viverra, fringilla augue. Sed vulputate sit amet nunc eu pharetra. Sed aliquam nisi at mi pharetra, ut consectetur est venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam felis quam, placerat sit amet urna eget, rhoncus rutrum neque. Sed quis tincidunt sapien. Nam a cursus nulla, et pellentesque elit.
+      </p>
+      <p>
+        Donec ac mi vel nisi semper congue. Curabitur blandit magna ac orci accumsan scelerisque. Suspendisse id est eget mi efficitur lobortis non sed risus. Fusce maximus vehicula lacinia. Nam eleifend eget lorem non rhoncus. Maecenas pellentesque arcu nisi, vel fringilla enim placerat id. Donec nec tellus laoreet erat consectetur malesuada. Fusce eu porta tortor. Duis in ante ultricies, semper ligula et, ultricies massa.
+      </p>
+  `,
   },
 };
 
--- router/course.handler.js
+++ router/course.handler.js
@@ -29,7 +29,7 @@ export default async function handleCourse(req, res) {
       message: "Página não encontrada",
     });
 
-  const [layout, cursemyCardTemplate] = await Promise.all([
+  const [layout, cursemyCardTemplate, courseContentRaw] = await Promise.all([
     readFile(
       path.join(__dirname, "../internal/components/layout.html"),
       "utf-8",
@@ -38,11 +38,31 @@ export default async function handleCourse(req, res) {
       path.join(__dirname, "../internal/components/cursemy-card.html"),
       "utf-8",
     ),
+    readFile(
+      path.join(__dirname, "../internal/components/course.html"),
+      "utf-8",
+    ),
   ]);
+
+  const courseContent = courseContentRaw
+    .replaceAll("{{COURSE_TITLE}}", course.title)
+    .replaceAll("{{COURSE_AUTHOR}}", course.author)
+    .replaceAll("{{COURSE_RATINGS_COUNT}}", course.rankings.count)
+    .replaceAll("{{COURSE_FULL_PRICE}}", course.price.full)
+    .replaceAll("{{COURSE_PRICE_WITH_DISCOUNT}}", course.price.discounted)
+    .replaceAll("{{COURSE_START_AT}}", course.startAt)
+    .replaceAll("{{COURSE_DURATION}}", course.duration)
+    .replaceAll("{{COURSE_SLUG}}", slug)
+    .replaceAll("{{COURSE_STARS}}", course.rankings.stars)
+    .replaceAll("{{COURSE_HIGHLIGHT}}", course.highlight)
+    .replaceAll(
+      "{{COURSE_DESCRIPTION}}",
+      course.description ?? "Lorem ipsum dolor sit amet",
+    );
 
   const page = layout
     .replace("{{title}}", `Cursemy - ${course.title}`)
-    .replace("{{content}}", `${cursemyCardTemplate}`)
+    .replace("{{content}}", `${cursemyCardTemplate}${courseContent}`)
     .replace(/\s{2,}/gi, " ");
 
   res.setHeader("Content-Type", "text/html");
